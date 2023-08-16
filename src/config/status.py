328ee93e1ea6d06a157dc84060aff0c6a397d57c
YingJie Chen <117566465+FalleNSakura2002@users.noreply.github.com>
2023-08-16 17:48:36 +0800

    Update status.py

--- src/config/status.py
+++ src/config/status.py
@@ -15,20 +15,20 @@ from enum import Enum
 
 class TimeStandardStatus(Enum):
     """
-        时间标准化状态提示
-    """
-    unrecognized = '1-unrecognized nerTime'  # 未识别到标准化时间-无效解析
-    Not_time_point = '1-Not Time Point'# 标准化类型不为time_point 或是 time_span
+    ner state enumeration
+"""
+    unrecognized = '1-unrecognized nerTime' # Normalized time not recognized - invalid parsing
+    not_time_point = '1-Not Time Point' # Standardized type not time_point or time_span
     success = '2-Success Parse'
 
 class SpaceStandardStatus(Enum):
-    miss_gpe = '0-Missing GPE'  # 丢失GPE信息
-    foreign = '0-Foreign'  # 国外地址
-    nodata = '0-NoDATA'  # 无数据
+    miss_gpe = '0-Missing GPE' # Missing GPE information
+    foreign = '0-Foreign' # Foreign address
+    nodata = '0-NoDATA' # no data
 
-    invalid = '1-Invalid Parsing'  # 无效解析
-    miss_part_gpe = '1-Missing part of gpe'  # 丢失部分行政区信息(如省、市、区)
-    uncertain = '1-Uncertain'  # 不确定是否解析正确
+    invalid = '1-Invalid Parsing' # invalid parsing
+    miss_part_gpe = '1-Missing part of gpe' # Missing part of gpe (e.g. province, city, district)
+    uncertain = '1-Uncertain' # Uncertain whether parsing is correct or not
 
     success = '2-Success Parse'
     common = '2-Common Parse'
